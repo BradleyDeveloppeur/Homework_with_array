ed55345956b3db2e00ba68361c5995a0d12e5c40
BradleyDeveloppeur <julienbradleycold08@gmail.com>
2023-09-27 19:50:23 -0300

    Vente 3

---
+++ index.js
@@ -0,0 +1,15 @@
+listeItem = []
+function additionner(){
+    let item = document.getElementById("item").value
+    listeItem.push(item)
+    nettoyer()
+    imprimer()
+}
+
+function nettoyer(){
+    document.getElementById("item").value=""
+}
+
+function imprimer(){
+    document.getElementById("liste").innerText = listeItem.join(" / ")
+}
